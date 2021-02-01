16321a31190ba1ea66e76175b7b6f7b42fdee261
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-01 14:24:25 +0100

    Added test_create_order_with_bool_as_divided

--- page/tests.py
+++ page/tests.py
@@ -941,3 +941,32 @@ class ActiveOrderTestCase(TestCase):
         self.assertEqual(active_order.divided, '-1.1')
         self.assertEqual(active_order.tracking_number, '0123456789012345678901')
         self.assertEqual(ActiveOrder.objects.count(), 2)
+
+    def test_create_order_with_bool_as_divided(self):
+        active_order = ActiveOrder(owner=self.groups[0], order_number='QWERTYUIOP1234', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided=True,
+                                   tracking_number='0123456789012345678901')
+        active_order.save()
+        active_order = ActiveOrder.objects.get(order_number='QWERTYUIOP1234')
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, 'QWERTYUIOP1234')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'True')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 1)
+
+        active_order = ActiveOrder(owner=self.groups[0], order_number='QWERTYUIOP1235', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided=False,
+                                   tracking_number='0123456789012345678901')
+        active_order.save()
+        active_order = ActiveOrder.objects.get(order_number='QWERTYUIOP1235')
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, 'QWERTYUIOP1235')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'False')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 2)
