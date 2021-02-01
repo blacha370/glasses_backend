bd920b6e432bfde92b9941256986a4df2d64cf2d
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-01 14:30:22 +0100

    Created test_create_order_without_divided

--- page/tests.py
+++ page/tests.py
@@ -1035,3 +1035,17 @@ class ActiveOrderTestCase(TestCase):
         self.assertEqual(active_order.divided, 'set()')
         self.assertEqual(active_order.tracking_number, '0123456789012345678901')
         self.assertEqual(ActiveOrder.objects.count(), 4)
+
+    def test_create_order_without_divided(self):
+        active_order = ActiveOrder(owner=self.groups[0], order_number='QWERTYUIOP1234', pub_date='01.01.2020',
+                                   order_status='1', image='000', tracking_number='0123456789012345678901')
+        active_order.save()
+        active_order = ActiveOrder.objects.get(order_number='QWERTYUIOP1234')
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, 'QWERTYUIOP1234')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, '?')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 1)
