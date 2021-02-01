4eed2b0efa50a975b2caac6a0918990114459a9f
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-01 14:59:37 +0100

    Created test_create_with_bool_as_tracking_number

--- page/tests.py
+++ page/tests.py
@@ -1169,3 +1169,30 @@ class ActiveOrderTestCase(TestCase):
         self.assertEqual(active_order.divided, 'całe')
         self.assertEqual(active_order.tracking_number, '-1.1')
         self.assertEqual(ActiveOrder.objects.count(), 2)
+
+    def test_create_with_bool_as_tracking_number(self):
+        active_order = ActiveOrder(owner=self.groups[0], order_number='QWERTYUIOP1234', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided='całe', tracking_number=True)
+        active_order.save()
+        active_order = ActiveOrder.objects.get(order_number='QWERTYUIOP1234')
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, 'QWERTYUIOP1234')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'całe')
+        self.assertEqual(active_order.tracking_number, 'True')
+        self.assertEqual(ActiveOrder.objects.count(), 1)
+
+        active_order = ActiveOrder(owner=self.groups[0], order_number='QWERTYUIOP1235', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided='całe', tracking_number=False)
+        active_order.save()
+        active_order = ActiveOrder.objects.get(order_number='QWERTYUIOP1235')
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, 'QWERTYUIOP1235')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'całe')
+        self.assertEqual(active_order.tracking_number, 'False')
+        self.assertEqual(ActiveOrder.objects.count(), 2)
