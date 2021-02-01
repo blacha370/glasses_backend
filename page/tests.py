d74d7c8d5ab3a98b9970f74a1146fddaa7f4eac7
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-01 12:33:11 +0100

    Created test_create_with_string_as_order_number

--- page/tests.py
+++ page/tests.py
@@ -108,3 +108,69 @@ class ActiveOrderTestCase(TestCase):
                                 divided='całe', tracking_number='0123456789012345678901')
             self.assertRaises(IntegrityError, order.save)
         self.assertEqual(ActiveOrder.objects.count(), 0)
+
+    def test_create_with_string_as_order_number(self):
+        active_order = ActiveOrder(owner=self.groups[0], order_number='', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided='całe',
+                                   tracking_number='0123456789012345678901')
+        active_order.save()
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, '')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'całe')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 1)
+
+        active_order = ActiveOrder(owner=self.groups[0], order_number=' ', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided='całe',
+                                   tracking_number='0123456789012345678901')
+        active_order.save()
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, ' ')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'całe')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 2)
+
+        active_order = ActiveOrder(owner=self.groups[0], order_number='\n ', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided='całe',
+                                   tracking_number='0123456789012345678901')
+        active_order.save()
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, '\n ')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'całe')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 3)
+
+        active_order = ActiveOrder(owner=self.groups[0], order_number='Order number', pub_date='01.01.2020',
+                                   order_status='1', image='000', divided='całe',
+                                   tracking_number='0123456789012345678901')
+        active_order.save()
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, 'Order number')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'całe')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 4)
+
+        active_order = ActiveOrder(owner=self.groups[0], order_number='Definitely too long order number',
+                                   pub_date='01.01.2020', order_status='1', image='000', divided='całe',
+                                   tracking_number='0123456789012345678901')
+        active_order.save()
+        self.assertEqual(active_order.owner.name, 'z4l')
+        self.assertEqual(active_order.order_number, 'Definitely too long order number')
+        self.assertEqual(active_order.pub_date, '01.01.2020')
+        self.assertEqual(active_order.order_status, '1')
+        self.assertEqual(active_order.image, '000')
+        self.assertEqual(active_order.divided, 'całe')
+        self.assertEqual(active_order.tracking_number, '0123456789012345678901')
+        self.assertEqual(ActiveOrder.objects.count(), 5)
