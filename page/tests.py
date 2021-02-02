becb0dc30e343d8975eff3983e5603c33eb72dd6
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-02 08:05:23 +0100

    Created test_update_status_with_int_as_new_status

--- page/tests.py
+++ page/tests.py
@@ -1558,3 +1558,18 @@ class ActiveOrderTestCase(TestCase):
         self.assertFalse(active_order.update_status("('2', 'W przygotowaniu')"))
         self.assertEqual(active_order.order_status, ('1', 'Nowe'))
 
+    def test_update_status_with_int_as_new_status(self):
+        active_order = ActiveOrder(owner=self.groups[0], order_number='QWERTYUIOP1234', pub_date='01.01.2020',
+                                   image='000', divided='całe', tracking_number='0123456789012345678901')
+        active_order.save()
+        self.assertFalse(active_order.update_status(1))
+        self.assertEqual(active_order.order_status, ('1', 'Nowe'))
+
+        self.assertFalse(active_order.update_status(0))
+        self.assertEqual(active_order.order_status, ('1', 'Nowe'))
+
+        self.assertFalse(active_order.update_status(-1))
+        self.assertEqual(active_order.order_status, ('1', 'Nowe'))
+
+        self.assertFalse(active_order.update_status(9999))
+        self.assertEqual(active_order.order_status, ('1', 'Nowe'))
