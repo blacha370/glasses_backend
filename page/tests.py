c8f6ca101836a5aca97428ac26a2f87aa7102615
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-02 10:18:13 +0100

    Added test_create_with_int_as_owner

--- page/tests.py
+++ page/tests.py
@@ -1528,3 +1528,18 @@ class UnactiveOrderTestCase(TestCase):
                                        pub_date=self.active_order.pub_date, image=self.active_order.image)
         self.assertEqual(UnactiveOrder.objects.count(), 0)
 
+    def test_create_with_int_as_owner(self):
+        with atomic():
+            self.assertRaises(ValueError, UnactiveOrder, owner=1, order_number=self.active_order.order_number,
+                                       pub_date=self.active_order.pub_date, image=self.active_order.image)
+        self.assertEqual(UnactiveOrder.objects.count(), 0)
+
+        with atomic():
+            self.assertRaises(ValueError, UnactiveOrder, owner=0, order_number=self.active_order.order_number,
+                                       pub_date=self.active_order.pub_date, image=self.active_order.image)
+        self.assertEqual(UnactiveOrder.objects.count(), 0)
+
+        with atomic():
+            self.assertRaises(ValueError, UnactiveOrder, owner=-1, order_number=self.active_order.order_number,
+                                       pub_date=self.active_order.pub_date, image=self.active_order.image)
+        self.assertEqual(UnactiveOrder.objects.count(), 0)
