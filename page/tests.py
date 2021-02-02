c127f3f858665738ac48f7ae83bfc19b5f5bfecf
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-02 07:34:43 +0100

    Created test_create_with_none_as_order_status

--- page/tests.py
+++ page/tests.py
@@ -1428,3 +1428,12 @@ class ActiveOrderTestCase(TestCase):
         self.assertEqual(active_order.divided, 'całe')
         self.assertEqual(active_order.tracking_number, '0123456789012345678901')
         self.assertEqual(ActiveOrder.objects.count(), 2)
+
+    def test_create_with_none_as_order_status(self):
+        with atomic():
+            active_order = ActiveOrder(owner=self.groups[0], order_number='QWERTYUIOP1234', pub_date='01.01.2020',
+                                       order_status=None, image='000', divided='całe',
+                                       tracking_number='0123456789012345678901')
+            self.assertRaises(IntegrityError, active_order.save)
+        self.assertEqual(ActiveOrder.objects.count(), 0)
+    
