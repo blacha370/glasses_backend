19e99bd601c1c50d26fcde21072f00c795b3313a
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-02 10:37:43 +0100

    Added test_create_with_int_as_order_number

--- page/tests.py
+++ page/tests.py
@@ -1629,3 +1629,25 @@ class UnactiveOrderTestCase(TestCase):
         unactive_order = UnactiveOrder.objects.get(pk=unactive_order.pk)
         self.assertEqual(UnactiveOrder.objects.count(), 4)
         self.assertEqual(unactive_order.order_number, 'number')
+
+    def test_create_with_int_as_order_number(self):
+        unactive_order = UnactiveOrder(owner=self.active_order.owner, order_number=1,
+                                       pub_date=self.active_order.pub_date, image=self.active_order.image)
+        unactive_order.save()
+        unactive_order = UnactiveOrder.objects.get(pk=unactive_order.pk)
+        self.assertEqual(UnactiveOrder.objects.count(), 1)
+        self.assertEqual(unactive_order.order_number, '1')
+
+        unactive_order = UnactiveOrder(owner=self.active_order.owner, order_number=0,
+                                       pub_date=self.active_order.pub_date, image=self.active_order.image)
+        unactive_order.save()
+        unactive_order = UnactiveOrder.objects.get(pk=unactive_order.pk)
+        self.assertEqual(UnactiveOrder.objects.count(), 2)
+        self.assertEqual(unactive_order.order_number, '0')
+
+        unactive_order = UnactiveOrder(owner=self.active_order.owner, order_number=-1,
+                                       pub_date=self.active_order.pub_date, image=self.active_order.image)
+        unactive_order.save()
+        unactive_order = UnactiveOrder.objects.get(pk=unactive_order.pk)
+        self.assertEqual(UnactiveOrder.objects.count(), 3)
+        self.assertEqual(unactive_order.order_number, '-1')
