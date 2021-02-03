8ff828f20ce912f52854fabac7fc4f95f7eacd9d
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-03 09:20:05 +0100

    Deleted unexisting models from admin site

--- page/admin.py
+++ page/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ActiveOrder, UnactiveOrder, OrderStatusChange, MessagesThread, Message, Notification, ArchiveThread, ArchiveMessage
+from .models import ActiveOrder, UnactiveOrder, OrderStatusChange, MessagesThread, Message, Notification
 
 admin.site.register(ActiveOrder)
 admin.site.register(UnactiveOrder)
@@ -7,5 +7,3 @@ admin.site.register(OrderStatusChange)
 admin.site.register(MessagesThread)
 admin.site.register(Message)
 admin.site.register(Notification)
-admin.site.register(ArchiveThread)
-admin.site.register(ArchiveMessage)
