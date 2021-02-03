04241750a3cd7c4727e668fff873c1673c88d1bc
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-03 08:03:54 +0100

    Added test_create_with_bool_as_message_op

--- page/tests.py
+++ page/tests.py
@@ -2240,3 +2240,10 @@ class MessageTestCase(TestCase):
 
         self.assertRaises(ValueError, Message, thread=self.thread, message_op=-1.1, message_text='Text')
         self.assertEqual(Message.objects.count(), 0)
+
+    def test_create_with_bool_as_message_op(self):
+        self.assertRaises(ValueError, Message, thread=self.thread, message_op=True, message_text='Text')
+        self.assertEqual(Message.objects.count(), 0)
+
+        self.assertRaises(ValueError, Message, thread=self.thread, message_op=False, message_text='Text')
+        self.assertEqual(Message.objects.count(), 0)
