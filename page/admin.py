26986fbb97b39cc6398514c0f32bf7c43dcd5168
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-03 11:37:00 +0100

    Deletedt UnactiveOrder from admin site

--- page/admin.py
+++ page/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import ActiveOrder, UnactiveOrder, OrderStatusChange, MessagesThread, Message, Notification
+from .models import ActiveOrder, OrderStatusChange, MessagesThread, Message, Notification
 
 admin.site.register(ActiveOrder)
-admin.site.register(UnactiveOrder)
 admin.site.register(OrderStatusChange)
 admin.site.register(MessagesThread)
 admin.site.register(Message)
