a65fca91b25ad67b9d0597369e96c48ced44bc53
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-04 10:01:52 +0100

    Created test_index_with_authenticate_without_group

--- page/tests/tests_views/tests_index_view.py
+++ page/tests/tests_views/tests_index_view.py
@@ -1,16 +1,18 @@
 from django.test import TestCase, Client
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group, AnonymousUser
 
 
 class IndexTestCase(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User(username='User', password='password')
-        admin_group = Group(name='administracja')
-        admin_group.save()
-        user_group = Group(name='druk')
-        user_group.save()
-        self.groups = {'administracja': admin_group, 'druk': user_group}
+        self.user.save()
+        names = ['administracja', 'druk', '4dich', 'besart', 'kasia', 'Pomoc techniczna']
+        self.groups = {}
+        for name in names:
+            group = Group(name=name)
+            group.save()
+            self.groups[name] = group
 
     def test_index_without_authenticate(self):
         response = self.client.get('/', follow=True)
@@ -18,3 +20,17 @@ class IndexTestCase(TestCase):
         self.assertEqual(response.templates[1].name, 'page/base.html')
         self.assertEqual(len(response.redirect_chain), 0)
         self.assertEqual(response.request['PATH_INFO'], '/')
+        self.assertIsInstance(response.wsgi_request.user, AnonymousUser)
+
+    def test_index_with_authenticate_without_group(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/', follow=True)
+        self.assertEqual(response.templates[0].name, 'page/index.html')
+        self.assertEqual(response.templates[1].name, 'page/base.html')
+        self.assertEqual(len(response.redirect_chain), 2)
+        self.assertEqual(response.redirect_chain[0][0], '/o/')
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.redirect_chain[1][0], '/')
+        self.assertEqual(response.redirect_chain[1][1], 302)
+        self.assertEqual(response.request['PATH_INFO'], '/')
+        self.assertIsInstance(response.wsgi_request.user, AnonymousUser)
