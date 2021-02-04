4f77499a52284f8fd0d24b62e882ba457152d38f
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-04 10:13:01 +0100

    Added test_logout_with_authentication to LogoutUserTestCase

--- page/tests/tests_views/tests_logout_user_view.py
+++ page/tests/tests_views/tests_logout_user_view.py
@@ -16,3 +16,13 @@ class LogoutUserTestCase(TestCase):
         self.assertEqual(response.redirect_chain[0][0], '/')
         self.assertEqual(response.redirect_chain[0][1], 302)
         self.assertIsInstance(response.wsgi_request.user, AnonymousUser)
+
+    def test_logout_with_authentication(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/o/', follow=True)
+        self.assertEqual(response.templates[0].name, 'page/index.html')
+        self.assertEqual(response.templates[1].name, 'page/base.html')
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][0], '/')
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertIsInstance(response.wsgi_request.user, AnonymousUser)
