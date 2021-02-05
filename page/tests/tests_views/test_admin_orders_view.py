b697b41c2e742677eca97f61080700059c200187
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-05 09:04:53 +0100

    Created test_admin_orders_put_method_without_authentication

--- page/tests/tests_views/test_admin_orders_view.py
+++ page/tests/tests_views/test_admin_orders_view.py
@@ -310,3 +310,11 @@ class AdminOrdersTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'page/admin_orders.html')
         self.assertEqual(response.templates[1].name, 'page/base.html')
         self.assertEqual(len(response.redirect_chain), 0)
+
+    def test_admin_orders_put_method_without_authentication(self):
+        response = self.client.put('/orders/1/a/', follow=True)
+        self.assertEqual(response.templates[0].name, 'page/index.html')
+        self.assertEqual(response.templates[1].name, 'page/base.html')
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][0], '/?next=/orders/1/a/')
+        self.assertEqual(response.redirect_chain[0][1], 302)
