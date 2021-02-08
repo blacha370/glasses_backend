0dd6c8fce3273f18e708eb203d1d704d2d9a55a6
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-08 10:56:46 +0100

    Created test_with_authentication_as_druk_group_post_method

--- page/tests/tests_views/tests_change_view.py
+++ page/tests/tests_views/tests_change_view.py
@@ -474,3 +474,15 @@ class ChangeTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'page/index.html')
         self.assertEqual(response.templates[1].name, 'page/base.html')
         self.assertEqual(self.order.order_status, '1')
+
+    def test_with_authentication_as_druk_group_post_method(self):
+        self.user.groups.add(self.groups['druk'])
+        self.client.force_login(self.user)
+        response = self.client.post('/change/1/', follow=True, data={'value': '2'})
+        self.assertEqual(response.request['PATH_INFO'], '/orders/1/u/')
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][0], '/orders/1/u/')
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.templates[0].name, 'page/user_orders.html')
+        self.assertEqual(response.templates[1].name, 'page/base.html')
+        self.assertEqual(self.order.order_status, '1')
