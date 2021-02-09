bcdbd295797371d54ea3d8bd7272c531402d2d85
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-09 09:51:56 +0100

    Added test_with_authentication_without_group

--- page/tests/tests_views/tests_archive_inbox.py
+++ page/tests/tests_views/tests_archive_inbox.py
@@ -26,3 +26,15 @@ class ArchiveInboxTestCase(TestCase):
         self.assertEqual(response.redirect_chain[0][1], 302)
         self.assertEqual(response.templates[0].name, 'page/index.html')
         self.assertEqual(response.templates[1].name, 'page/base.html')
+
+    def test_with_authentication_without_group(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/archive/', follow=True)
+        self.assertEqual(response.request['PATH_INFO'], '/')
+        self.assertEqual(len(response.redirect_chain), 2)
+        self.assertEqual(response.redirect_chain[0][0], '/o/')
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.redirect_chain[1][0], '/')
+        self.assertEqual(response.redirect_chain[1][1], 302)
+        self.assertEqual(response.templates[0].name, 'page/index.html')
+        self.assertEqual(response.templates[1].name, 'page/base.html')
