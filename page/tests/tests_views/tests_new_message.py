3fc5d583f1b88c5817a2fc628afd1142b63002cd
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-10 13:02:28 +0100

    Created NewMessageTestCase

---
+++ page/tests/tests_views/tests_new_message.py
@@ -0,0 +1,22 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User, Group
+
+
+class NewMessageTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User(username='User', password='password')
+        self.user.save()
+        names = ['administracja', 'druk', '4dich', 'besart', 'kasia', 'Pomoc techniczna']
+        self.groups = {name: Group(name=name) for name in names}
+        for group in self.groups.values():
+            group.save()
+
+    def test_get_method_without_authentication(self):
+        response = self.client.get('/message/new/', follow=True)
+        self.assertEqual(response.request['PATH_INFO'], '/')
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][0], '/?next=/message/new/')
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.templates[0].name, 'page/index.html')
+        self.assertEqual(response.templates[1].name, 'page/base.html')
