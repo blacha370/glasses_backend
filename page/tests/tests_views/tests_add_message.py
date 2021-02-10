1ad9d28616ad3f7165e26e3abfac6e61de6033ad
blacha370 <57962549+blacha370@users.noreply.github.com>
2021-02-10 10:22:25 +0100

    Created AddMessageTestCase

---
+++ page/tests/tests_views/tests_add_message.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User, Group
+from ...models import MessagesThread, Message, Notification
+
+
+class AddMessageTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User(username='User', password='password')
+        self.user.save()
+        names = ['administracja', 'druk', '4dich', 'besart', 'kasia', 'Pomoc techniczna']
+        self.groups = {name: Group(name=name) for name in names}
+        for group in self.groups.values():
+            group.save()
+
+    def test_without_authentication(self):
+        response = self.client.post('/message/add/subject/', {'message_text': 'text'}, follow=True)
+        self.assertEqual(response.request['PATH_INFO'], '/')
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][0], '/?next=/message/add/subject/')
+        self.assertEqual(response.redirect_chain[0][1], 302)
+        self.assertEqual(response.templates[0].name, 'page/index.html')
+        self.assertEqual(response.templates[1].name, 'page/base.html')
